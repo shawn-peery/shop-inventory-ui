0b404a72e43c0f4b3b730ba526e55da20edcbbe3
ShawnWhitaker <shawn.w8465@gmail.com>
2021-02-27 18:56:02 -0700

    Successfully looking okay

--- src/components/forms/ResourceForm.js
+++ src/components/forms/ResourceForm.js
@@ -3,119 +3,119 @@ import React from "react";
 import { Redirect, Link } from "react-router-dom";
 import InputField from "./InputField";
 
-import { Form, Button } from "reactstrap";
+import { Form } from "reactstrap";
 
 function ResourceForm({
-  resourceName,
-  resourceFields,
-  stateFields,
-  setStateFields,
-  redirect,
-  setRedirect,
-  params,
-  create,
-  token,
+	resourceName,
+	resourceFields,
+	stateFields,
+	setStateFields,
+	redirect,
+	setRedirect,
+	params,
+	create,
+	token,
 }) {
-  const [backHome, setBackHome] = React.useState(false);
+	const [backHome, setBackHome] = React.useState(false);
 
-  const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-  const REACT_APP_RESOURCE_API_BASE_URL = process.env.REACT_APP_RESOURCE_API_BASE_URL.replace(
-    "<resource>",
-    // Perhaps in the future, will add functionality for resources that have differeing plural words
-    resourceName.toLowerCase() + "s"
-  );
+	const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
+	const REACT_APP_RESOURCE_API_BASE_URL = process.env.REACT_APP_RESOURCE_API_BASE_URL.replace(
+		"<resource>",
+		// Perhaps in the future, will add functionality for resources that have differeing plural words
+		resourceName.toLowerCase() + "s"
+	);
 
-  function onSubmit(event) {
-    event.preventDefault();
+	function onSubmit(event) {
+		event.preventDefault();
 
-    const form = document.getElementById("resource-form");
+		const form = document.getElementById("resource-form");
 
-    const bodyObj = {};
+		const bodyObj = {};
 
-    const inputs = form.querySelectorAll("input");
+		const inputs = form.querySelectorAll("input");
 
-    inputs.forEach((input) => {
-      if (!input.hasAttribute("data-form-group")) {
-        bodyObj[input.name] = input.value;
-        return;
-      }
+		inputs.forEach((input) => {
+			if (!input.hasAttribute("data-form-group")) {
+				bodyObj[input.name] = input.value;
+				return;
+			}
 
-      if (input.name in bodyObj) {
-        const formGroup = bodyObj[input.name];
-        formGroup.push(input.value);
-        bodyObj[input.name] = formGroup;
-      } else {
-        const newFormGroup = [input.value];
-        bodyObj[input.name] = newFormGroup;
-      }
-    });
+			if (input.name in bodyObj) {
+				const formGroup = bodyObj[input.name];
+				formGroup.push(input.value);
+				bodyObj[input.name] = formGroup;
+			} else {
+				const newFormGroup = [input.value];
+				bodyObj[input.name] = newFormGroup;
+			}
+		});
 
-    let url;
+		let url;
 
-    if (!create) {
-      url = `${REACT_APP_API_URL}${REACT_APP_RESOURCE_API_BASE_URL}/${params.id}`;
-    } else {
-      url = `${REACT_APP_API_URL}${REACT_APP_RESOURCE_API_BASE_URL}`;
-    }
+		if (!create) {
+			url = `${REACT_APP_API_URL}${REACT_APP_RESOURCE_API_BASE_URL}/${params.id}`;
+		} else {
+			url = `${REACT_APP_API_URL}${REACT_APP_RESOURCE_API_BASE_URL}`;
+		}
 
-    fetch(url, {
-      method: create ? "POST" : "PUT",
-      headers: {
-        auth: token,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bodyObj),
-    });
+		fetch(url, {
+			method: create ? "POST" : "PUT",
+			headers: {
+				auth: token,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(bodyObj),
+		});
 
-    setRedirect(true);
-  }
+		setRedirect(true);
+	}
 
-  const inputFields = [];
+	const inputFields = [];
 
-  generateInputFields();
+	generateInputFields();
 
-  function generateInputFields() {}
+	function generateInputFields() {}
 
-  return (
-    <>
-      <Form id="resource-form" onSubmit={onSubmit}>
-        {resourceFields.map((resourceField, index) => {
-          return (
-            <InputField
-              key={index}
-              name={resourceField.name}
-              inputType={resourceField.inputType}
-              currency={resourceField.currency}
-              stateValue={stateFields[resourceField.name]}
-              stateFields={stateFields}
-              setStateFields={setStateFields}
-              index={index}
-            />
-          );
-        })}
-        <Button
-          color="primary"
-          type="submit"
-          className="submit-button"
-          onClick={onSubmit}
-        >
-          Submit
-        </Button>
-      </Form>
-      <Link to="/">
-        <Button
-          onClick={() => {
-            setBackHome(true);
-          }}
-        >
-          Back To Home Page
-        </Button>
-      </Link>
-      {backHome && <Redirect to="/" push />}
-      {redirect && <Redirect to="/" push />}
-    </>
-  );
+	return (
+		<>
+			<Form id="resource-form" onSubmit={onSubmit}>
+				{resourceFields.map((resourceField, index) => {
+					return (
+						<InputField
+							key={index}
+							name={resourceField.name}
+							inputType={resourceField.inputType}
+							currency={resourceField.currency}
+							stateValue={stateFields[resourceField.name]}
+							stateFields={stateFields}
+							setStateFields={setStateFields}
+							index={index}
+						/>
+					);
+				})}
+				<button
+					color="primary"
+					type="submit"
+					className="submit-button"
+					onClick={onSubmit}
+				>
+					Submit
+				</button>
+			</Form>
+			<Link to="/">
+				<button
+					onClick={() => {
+						setBackHome(true);
+					}}
+				>
+					Back To Home Page
+				</button>
+			</Link>
+			{backHome && <Redirect to="/" push />}
+			{redirect && <Redirect to="/" push />}
+		</>
+	);
 }
 
 export default ResourceForm;
